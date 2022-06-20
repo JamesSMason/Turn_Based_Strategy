58dcafc71d83c6bc79bb65a11160766808c0f4ba
Number5IsAlive <james.s.mason@hotmail.com>
2022-06-20 16:47:15 +0100

    31 Camera Zoom

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,14 +1,36 @@
+using Cinemachine;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera = null;
     [SerializeField] private float cameraMoveSpeed = 10f;
     [SerializeField] private float cameraRotationSpeed = 100f;
+    [SerializeField] private float zoomFactor = 1f;
+    [SerializeField] private float zoomSpeed = 5f;
+
+    private CinemachineTransposer cinemachineTransposer;
+    private Vector3 targetFollowOffset;
+
+    private const float MIN_FOLLOW_Y_OFFSET = 2f;
+    private const float MAX_FOLLOW_Y_OFFSET = 12f;
+
+    private void Start()
+    {
+        cinemachineTransposer = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        targetFollowOffset = cinemachineTransposer.m_FollowOffset;
+    }
 
     void Update()
     {
+        HandleMovement();
+        HandleRotation();
+        HandleZoom();
+    }
+
+    private void HandleMovement()
+    {
         Vector3 inputMoveDir = new Vector3(0, 0, 0);
-        Vector3 inputRotationDir = new Vector3(0, 0, 0);
 
         if (Input.GetKey(KeyCode.W))
         {
@@ -26,6 +48,14 @@ public class CameraController : MonoBehaviour
         {
             inputMoveDir.x = 1f;
         }
+
+        Vector3 moveVector = transform.forward * inputMoveDir.z + transform.right * inputMoveDir.x;
+        transform.position += moveVector * cameraMoveSpeed * Time.deltaTime;
+    }
+
+    private void HandleRotation()
+    {
+        Vector3 inputRotationDir = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.Q))
         {
             inputRotationDir.y = 1f;
@@ -34,9 +64,22 @@ public class CameraController : MonoBehaviour
         {
             inputRotationDir.y = -1f;
         }
-
-        Vector3 moveVector = transform.forward * inputMoveDir.z + transform.right * inputMoveDir.x;
-        transform.position += moveVector * cameraMoveSpeed * Time.deltaTime;
         transform.eulerAngles += inputRotationDir * cameraRotationSpeed * Time.deltaTime;
+    }
+
+    private void HandleZoom()
+    {
+        if (Input.mouseScrollDelta.y > 0f)
+        {
+            targetFollowOffset.y -= zoomFactor;
+        }
+
+        if (Input.mouseScrollDelta.y < 0f)
+        {
+            targetFollowOffset.y += zoomFactor;
+        }
+        targetFollowOffset.y = Mathf.Clamp(targetFollowOffset.y, MIN_FOLLOW_Y_OFFSET, MAX_FOLLOW_Y_OFFSET);
+
+        cinemachineTransposer.m_FollowOffset = Vector3.Lerp(cinemachineTransposer.m_FollowOffset, targetFollowOffset, Time.deltaTime * zoomSpeed);
     }
 }

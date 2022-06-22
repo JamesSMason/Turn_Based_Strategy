2cc0e97a810e30e338aa33a9e465b4e6cc144942
Number5IsAlive <james.s.mason@hotmail.com>
2022-06-22 08:26:55 +0100

    37 Single Active Action

--- Assets/Scripts/Actions/SpinAction.cs
+++ Assets/Scripts/Actions/SpinAction.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class SpinAction : BaseAction
@@ -15,11 +16,13 @@ public class SpinAction : BaseAction
         if (totalSpinAmount >= 360f)
         {
             isActive = false;
+            onActionComplete();
         }
     }
 
-    public void Spin()
+    public void Spin(Action onActionComplete)
     {
+        this.onActionComplete = onActionComplete;
         isActive = true;
         totalSpinAmount = 0f;
     }

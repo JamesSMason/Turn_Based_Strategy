a12353df76f04dfd66c26e29a15f212809844de9
Number5IsAlive <james.s.mason@hotmail.com>
2022-07-02 15:47:11 +0100

    77 Input System

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,8 +1,12 @@
+#define USE_NEW_INPUT_SYSTEM
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
     public static InputManager Instance { get; private set; }
+
+    private PlayerInputActions playerInputActions;
 
     private void Awake()
     {
@@ -14,11 +18,18 @@ public class InputManager : MonoBehaviour
         }
 
         Instance = this;
+
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Enable();
     }
 
     public Vector2 GetMouseScreenPosition()
     {
+#if USE_NEW_INPUT_SYSTEM
+        return Mouse.current.position.ReadValue();
+#else
         return Input.mousePosition;
+#endif
     }
 
     public bool IsMouseButtonDown()
@@ -28,6 +39,9 @@ public class InputManager : MonoBehaviour
 
     public Vector2 GetCameraMoveVector()
     {
+#if USE_NEW_INPUT_SYSTEM
+        return playerInputActions.Player.CameraMovement.ReadValue<Vector2>();
+#else
         Vector2 inputMoveDir = new Vector2(0, 0);
 
         if (Input.GetKey(KeyCode.W))
@@ -48,10 +62,14 @@ public class InputManager : MonoBehaviour
         }
 
         return inputMoveDir;
+#endif
     }
 
     public float GetCameraRotateAmount()
     {
+#if USE_NEW_INPUT_SYSTEM
+        return playerInputActions.Player.CameraRotate.ReadValue<float>();
+#else
         float rotateAmount = 0f;
 
         if (Input.GetKey(KeyCode.Q))
@@ -64,10 +82,14 @@ public class InputManager : MonoBehaviour
         }
 
         return rotateAmount;
+#endif
     }
 
     public float GetCameraZoomAmount()
     {
+#if USE_NEW_INPUT_SYSTEM
+        return playerInputActions.Player.CameraZoom.ReadValue<float>();
+#else
         float zoomAmount = 0f;
         
         if (Input.mouseScrollDelta.y > 0f)
@@ -81,5 +103,6 @@ public class InputManager : MonoBehaviour
         }
 
         return zoomAmount;
+#endif
     }
 }
